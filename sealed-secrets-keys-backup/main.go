07135e0e887f687c776a021d39734ebbb8af4a31
Brennon Loveless <brennon.loveless@gmail.com>
2022-05-19 21:49:07 -0700

    Removed unused code

--- sealed-secrets-keys-backup/main.go
+++ sealed-secrets-keys-backup/main.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
-	"net/http"
 	"time"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -19,16 +18,16 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
+
 	// creates the clientset
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		panic(err.Error())
 	}
 
+	namespace := "kube-system"
+
 	for {
-		// get pods in all the namespaces by omitting namespace
-		// Or specify namespace to get pods in particular namespace
-		namespace := "kube-system"
 		secrets, err := clientset.CoreV1().Secrets(namespace).List(context.TODO(), metav1.ListOptions{
 			LabelSelector: "sealedsecrets.bitnami.com/sealed-secrets-key",
 		})
@@ -49,27 +48,6 @@ func main() {
 
 		log.Println(string(secretsBytesYaml))
 
-		// // Examples for error handling:
-		// // - Use helper functions e.g. errors.IsNotFound()
-		// // - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
-		// _, err = clientset.CoreV1().Pods("default").Get(context.TODO(), "example-xxxxx", metav1.GetOptions{})
-		// if errors.IsNotFound(err) {
-		// 	fmt.Printf("Pod example-xxxxx not found in default namespace\n")
-		// } else if statusError, isStatus := err.(*errors.StatusError); isStatus {
-		// 	fmt.Printf("Error getting pod %v\n", statusError.ErrStatus.Message)
-		// } else if err != nil {
-		// 	panic(err.Error())
-		// } else {
-		// 	fmt.Printf("Found example-xxxxx pod in default namespace\n")
-		// }
-
 		time.Sleep(10 * time.Second)
-	}
-
-	fmt.Println("Hello from sealed secrets keys backup. Again. Yet again")
-
-	log.Println("Listening on :8090")
-	if err := http.ListenAndServe(":8090", nil); err != nil {
-		log.Fatalf("Error listening on :8090, err: %v", err)
 	}
 }
