54fb26926f7676f48e0fd0131406ec1e97e21b43
Brennon Loveless <brennon.loveless@gmail.com>
2023-04-16 09:50:54 -0700

    Minimal shell.nix

--- homelab-exporter-rs/shell.nix
+++ homelab-exporter-rs/shell.nix
@@ -3,7 +3,7 @@
     buildInputs = with pkgs; [
       # clang
       # llvmPackages.bintools
-      rustup
+      # rustup
       pkg-config
       openssl
     ];
@@ -15,16 +15,17 @@
     #   export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
     #   '';
     # Add libvmi precompiled library to rustc search path
-    # RUSTFLAGS = (builtins.map (a: ''-L ${a}/lib'') [
-    #   pkgs.openssl.dev
+    RUSTFLAGS = (builtins.map (a: ''-L ${a}/lib'') [
+      pkgs.openssl.dev
+    ]);
 
     # BINDGEN_EXTRA_CLANG_ARGS =
-    # # Includes with normal include path
+    # # # Includes with normal include path
     # (builtins.map (a: ''-I"${a}/include"'') [
     #   # pkgs.libvmi
-    #   pkgs.glibc.dev
-    #   # pkgs.openssl.dev
-    # ])
+    #   # pkgs.glibc.dev
+    #   pkgs.openssl.dev
+    # ]);
     # # Includes with special directory paths
     # ++ [
     #   ''-I"${pkgs.llvmPackages_latest.libclang.lib}/lib/clang/${pkgs.llvmPackages_latest.libclang.version}/include"''
