52eaa361df52a67b4fb77bb3f0f7d583b53f59f0
Brennon Loveless <brennon.loveless@gmail.com>
2023-04-16 09:41:07 -0700

    Remove unnecessary shell.nix declarations

--- homelab-exporter-rs/shell.nix
+++ homelab-exporter-rs/shell.nix
@@ -1,19 +1,19 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell rec {
     buildInputs = with pkgs; [
-      clang
-      llvmPackages.bintools
+      # clang
+      # llvmPackages.bintools
       rustup
       pkg-config
       openssl
     ];
-    RUSTC_VERSION = pkgs.lib.readFile ./rust-toolchain;
-    # https://github.com/rust-lang/rust-bindgen#environment-variables
-    LIBCLANG_PATH = pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
-    shellHook = ''
-      export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
-      export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
-      '';
+    # RUSTC_VERSION = pkgs.lib.readFile ./rust-toolchain;
+    # # https://github.com/rust-lang/rust-bindgen#environment-variables
+    # LIBCLANG_PATH = pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
+    # shellHook = ''
+    #   export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
+    #   export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
+    #   '';
     # Add libvmi precompiled library to rustc search path
     # RUSTFLAGS = (builtins.map (a: ''-L ${a}/lib'') [
     #   pkgs.openssl.dev
@@ -31,5 +31,4 @@
     #   ''-I"${pkgs.glib.dev}/include/glib-2.0"''
     #   ''-I${pkgs.glib.out}/lib/glib-2.0/include/''
     # ];
-
   }
