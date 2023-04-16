78b5ad8f7f19aed82d870efa00c9cb566b8dcf6b
Brennon Loveless <brennon.loveless@gmail.com>
2023-04-16 09:29:52 -0700

    New shell.nix

--- homelab-exporter-rs/shell.nix
+++ homelab-exporter-rs/shell.nix
@@ -18,18 +18,18 @@
     # RUSTFLAGS = (builtins.map (a: ''-L ${a}/lib'') [
     #   pkgs.openssl.dev
 
-    BINDGEN_EXTRA_CLANG_ARGS = 
-    # Includes with normal include path
-    (builtins.map (a: ''-I"${a}/include"'') [
-      # pkgs.libvmi
-      # pkgs.glibc.dev 
-      # pkgs.openssl.dev 
-    ])
-    # Includes with special directory paths
-    ++ [
-      ''-I"${pkgs.llvmPackages_latest.libclang.lib}/lib/clang/${pkgs.llvmPackages_latest.libclang.version}/include"''
-      ''-I"${pkgs.glib.dev}/include/glib-2.0"''
-      ''-I${pkgs.glib.out}/lib/glib-2.0/include/''
-    ];
+    # BINDGEN_EXTRA_CLANG_ARGS =
+    # # Includes with normal include path
+    # (builtins.map (a: ''-I"${a}/include"'') [
+    #   # pkgs.libvmi
+    #   pkgs.glibc.dev
+    #   # pkgs.openssl.dev
+    # ])
+    # # Includes with special directory paths
+    # ++ [
+    #   ''-I"${pkgs.llvmPackages_latest.libclang.lib}/lib/clang/${pkgs.llvmPackages_latest.libclang.version}/include"''
+    #   ''-I"${pkgs.glib.dev}/include/glib-2.0"''
+    #   ''-I${pkgs.glib.out}/lib/glib-2.0/include/''
+    # ];
 
   }
